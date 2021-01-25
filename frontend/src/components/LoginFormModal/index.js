ea80fb17649ab385f29af7c47a6d009d931f117e
Taten <knight.taten@gmail.com>
2021-01-25 16:09:09 -0600

    Changes for init commit

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import LoginForm from './LoginForm';
 
+// Hello
 function LoginFormModal() {
   const [showModal, setShowModal] = useState(false);
 
