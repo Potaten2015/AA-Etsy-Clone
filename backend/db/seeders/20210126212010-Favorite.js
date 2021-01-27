97cb19518a886d2e240a3917069e2e42515a94b4
Taten <knight.taten@gmail.com>
2021-01-27 04:24:51 -0600

    Finished seed data

--- backend/db/seeders/20210126212010-Favorite.js
+++ backend/db/seeders/20210126212010-Favorite.js
@@ -2,25 +2,91 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+   return queryInterface.bulkInsert('Favorites', [
+    {
+      userId: 1,
+      itemId: 1,
+    },
+    {
+      userId: 1,
+      itemId: 2,
+    },
+    {
+      userId: 2,
+      itemId: 2,
+    },
+    {
+      userId: 3,
+      itemId: 2,
+    },
+    {
+      userId: 6,
+      itemId: 5,
+    },
+    {
+      userId: 5,
+      itemId: 5,
+    },
+    {
+      userId: 4,
+      itemId: 12,
+    },
+    {
+      userId: 3,
+      itemId: 11,
+    },
+    {
+      userId: 5,
+      itemId: 3,
+    },
+    {
+      userId: 3,
+      itemId: 6,
+    },
+    {
+      userId: 2,
+      itemId: 3,
+    },
+    {
+      userId: 3,
+      itemId: 4,
+    },
+    {
+      userId: 5,
+      itemId: 4,
+    },
+    {
+      userId: 3,
+      itemId: 4,
+    },
+    {
+      userId: 5,
+      itemId: 6,
+    },
+    {
+      userId: 6,
+      itemId: 13,
+    },
+    {
+      userId: 2,
+      itemId: 13,
+    },
+    {
+      userId: 4,
+      itemId: 5,
+    },
+    {
+      userId: 4,
+      itemId: 15,
+    },
+    {
+      userId: 2,
+      itemId: 12,
+    },
+], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+   return queryInterface.bulkDelete('Favorites', null, {});
   }
 };
--- backend/db/seeders/20210126212019-Comment.js
+++ backend/db/seeders/20210126212019-Comment.js
@@ -2,25 +2,95 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+   return queryInterface.bulkInsert('Comments', [
+    {
+      userId: 1,
+      itemId: 1,
+      rating: 5,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 1,
+      itemId: 2,
+      rating: 5,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 2,
+      itemId: 3,
+      rating: 3,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 3,
+      itemId: 4,
+      rating: 1,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 1,
+      itemId: 1,
+      rating: 3,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 6,
+      itemId: 4,
+      rating: 2,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 6,
+      itemId: 3,
+      rating: 4,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 6,
+      itemId: 2,
+      rating: 4,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 5,
+      itemId: 7,
+      rating: 1,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 4,
+      itemId: 13,
+      rating: 2,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 5,
+      itemId: 12,
+      rating: 3,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+    {
+      userId: 6,
+      itemId: 16,
+      rating: 2,
+      title: 'Cool!',
+      content: 'Quite the product!',
+    },
+], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('Comments', null, {});
   }
 };
