1dd9b1704244bdf1464623750747d48667f29078
Taten <knight.taten@gmail.com>
2021-03-04 11:25:55 -0600

    Fixed demo login

--- frontend/src/components/EntryPage/DemoForm/index.js
+++ frontend/src/components/EntryPage/DemoForm/index.js
@@ -3,36 +3,47 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../../store/session";
 
-import '../EntryPage.css';
+import "../EntryPage.css";
 
-function DemoForm({setSignedIn}) {
-    const dispatch = useDispatch()
+function DemoForm({ setSignedIn }) {
+  const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
 
   if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    return dispatch(sessionActions.login({ email: "demo@user.io", password: "password" }))
-  }
+    return dispatch(
+      sessionActions.login({
+        credential: "demo@user.io",
+        password: "password",
+      }),
+    );
+  };
 
   return (
-        <form className="entry-form" onSubmit={handleSubmit}>
-          <table className="entry-form-table">
-            <thead>
-              <tr>
-                <th colSpan="2" className="entry-form-table-header">
-                  E N T E R • A S • D E M O • U S E R
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="entry-submit-container">
-                  <button onClick={handleSubmit} colSpan="2" className="entry-submit-button" type="submit" >E N T E R</button>
-              </tr>
-            </tbody>
-          </table>
-        </form>
+    <form className="entry-form" onSubmit={handleSubmit}>
+      <table className="entry-form-table">
+        <thead>
+          <tr>
+            <th colSpan="2" className="entry-form-table-header">
+              E N T E R • A S • D E M O • U S E R
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="entry-submit-container">
+            <button
+              onClick={handleSubmit}
+              colSpan="2"
+              className="entry-submit-button"
+              type="submit">
+              E N T E R
+            </button>
+          </tr>
+        </tbody>
+      </table>
+    </form>
   );
 }
 
